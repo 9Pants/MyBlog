ccb4e76888abe5a26d055ddd9935a7e1994ff56e
David Mitchell <D_R_mitchell@yahoo.com>
2020-03-23 21:29:34 +0000

    Add comments and style templates

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,39 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Post
 
-class BlogListView(ListView):
+class BlogListView(LoginRequiredMixin, ListView):
     model=Post
     template_name='blog.html'
+    login_url='account_login'
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model=Post
     template_name='post_detail.html'
+    login_url='account_login'
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model=Post
     template_name='post_new.html'
-    fields=['title','author','body']
+    fields=['title','body']
+    login_url='account_login'
 
-class BlogUpdateView(UpdateView):
+    def form_valid(self, form):
+        print(self.request.user)
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model=Post
     template_name='post_edit.html'
     fields=['title','body']
+    login_url='account_login'
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model=Post
     template_name='post_delete.html'
     success_url=reverse_lazy('post')
+    login_url='account_login'

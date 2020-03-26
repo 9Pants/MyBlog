08ed0da774492868aad1be368647916493dfce5d
David Mitchell <D_R_mitchell@yahoo.com>
2020-03-26 21:30:52 +0000

    database default update

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -100,14 +100,14 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
+    'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': env.str('DB_NAME'),
 #         'USER': env.str('DB_USERNAME'),
 #         'PASSWORD': env.str('DB_PASSWORD'),
 #         'HOST': env.str('DB_HOST'),
 #         'PORT': env.str('DB_PORT'),
-#     }
+    }
 }
 
 # Password validation
